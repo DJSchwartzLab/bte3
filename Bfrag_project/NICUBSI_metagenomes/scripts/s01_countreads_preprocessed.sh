96804bff568383ca0646052f875b8cb1f17635b8
itsjesstung <j.l.tung@wustl.edu>
2025-06-06 01:28:19 -0500

    Added files

---
+++ Bfrag_project/NICUBSI_metagenomes/scripts/s01_countreads_preprocessed.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#==================================================================================
+# File name    : s01_countreads_preprocessed.sh
+# Description  : This script will loop through a directory and count the number of 
+#                reads in FASTQ files.
+# Usage        : sbatch s01_countreads_preprocessed.sh
+# Author       : Jessica Tung (j.l.tung@wustl.edu)
+# Version      : 1.0
+# Created on   : 02/13/2025
+# Last modified: 02/13/2025
+#==================================================================================
+
+# Submission script for HTCF
+#SBATCH --job-name=s01_countreads_preprocessed.sh
+#SBATCH --time=0-00:00:00 # days-hh:mm:ss
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH --output=slurmout/countreads_preprocessed/x_countreads_preprocessed_%a.out
+#SBATCH --error=slurmout/countreads_preprocessed/y_countreads_preprocessed_%a.err
+
+# Define directories and output files
+BASEDIR=/scratch/djslab/jltung/Bfrag_project/NICUBSI_metagenomes
+INDIR=$BASEDIR/downloaded_reads
+OUTDIR=$BASEDIR/countreads_preprocessed
+FILENAMES=$OUTDIR/filenames.txt
+COUNT_FILE=$OUTDIR/readcounts.txt
+
+mkdir -p $OUTDIR
+
+# Loop through each FASTQ file and count reads
+for file in "$INDIR"/*FW_CLEAN.fastq; do
+    echo "$(basename "$file")" >> "$FILENAMES"
+    awk '{s++} END {print s/4}' "$file" >> "$COUNT_FILE"
+done
---
+++ Bfrag_project/NICUBSI_metagenomes/scripts/s11_bowtie2_bte3.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#===============================================================================
+# File name    : s10_bowtie2_bte3.sh
+# Description  : Align reads to a pre-indexed reference sequence using Bowtie 2.
+#                Designed to be run after index creation using bowtie2-build.
+# Usage        : sbatch s10_bowtie2.sh
+# Author       : Jessica Tung (j.l.tung@wustl.edu)
+# Version      : 1.0
+# Created on   : 02/13/2025
+# Last modified: 02/13/2025
+#===============================================================================
+
+# Submission script for HTCF
+#SBATCH --job-name=bowtie2
+#SBATCH --mem=8G
+#SBATCH --cpus-per-task=4
+#SBATCH --array=1-462
+#SBATCH --output=slurmout/bowtie2_bte3/x_bowtie2_bte3_%A_%a.out
+#SBATCH --error=slurmout/bowtie2_bte3/y_bowtie2_bte3_%A_%a.err
+
+# Load Miniconda using Spack
+eval $(spack load --sh miniconda3@4.10.3)
+
+# Activate the bowtie2 Conda environment
+source activate bowtie2
+
+#Define paths
+BASEDIR=/scratch/djslab/jltung/Bfrag_project
+READSDIR=${BASEDIR}/NICUBSI_metagenomes/bbmap_repaired
+SAMPLE=`sed -n ${SLURM_ARRAY_TASK_ID}p ${BASEDIR}/NICUBSI_metagenomes/NICUBSI_metagenomes_mappingfile.txt`
+REF=bte3
+REF_INDEX=${BASEDIR}/bte_reference_sequences/${REF}/${REF} # Specify the full path to the index prefix
+
+OUTDIR=${BASEDIR}/NICUBSI_metagenomes/bowtie2_${REF}/${SAMPLE}_${REF}
+
+# Make output directory
+mkdir -p ${OUTDIR}
+
+# Run Bowtie 2
+time bowtie2 -p ${SLURM_CPUS_PER_TASK} \
+    -x ${REF_INDEX} \
+    -S ${OUTDIR}/${SAMPLE}_mappedreads.sam \
+    -1 ${READSDIR}/${SAMPLE}_FW_CLEAN_REPAIRED.fastq \
+    -2 ${READSDIR}/${SAMPLE}_RV_CLEAN_REPAIRED.fastq
+
+RC=$?
+
+if [ "$RC" -eq 0 ]; then
+    echo "Bowtie2 alignment for ${SAMPLE} completed successfully."
+else
+    echo "Bowtie2 alignment for ${SAMPLE} failed with exit code $RC."
+    exit $RC
+fi
