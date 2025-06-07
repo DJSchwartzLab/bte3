7e52fd698ab8d75f90bcf489448fde95cb146265
itsjesstung <j.l.tung@wustl.edu>
2025-06-07 11:58:17 -0500

    Modified files

--- Bfrag_project/bfrag_isolates/bte1_3/scripts/s08_bowtie2_bte3.sh
+++ Bfrag_project/bfrag_isolates/bte1_3/scripts/s08_bowtie2_bte3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #===============================================================================
 # File name    : s07_bowtie2_bte3.sh
 # Description  : Align reads to a pre-indexed reference sequence using Bowtie 2.
