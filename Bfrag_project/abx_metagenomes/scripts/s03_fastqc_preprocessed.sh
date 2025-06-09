7c346453f48f7ec4086757e730671b3a0295d56a
itsjesstung <j.l.tung@wustl.edu>
2025-06-09 13:54:52 -0500

    Added file

---
+++ Bfrag_project/abx_metagenomes/scripts/s03_fastqc_preprocessed.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#==================================================================================
+# File name    : s03_fastqc_preprocessed.sh
+# Description  : This script will run FastQC on samples in parallel.
+# Usage        : sbatch s03_fastqc_preprocessed.sh
+# Author       : Jessica Tung (j.l.tung@wustl.edu)
+# Version      : 1.0
+# Created on   : 02/13/2025
+# Last modified: 02/13/2025
+#==================================================================================
+
+# Submission script for HTCF
+#SBATCH --job-name=fastqc_preprocessed
+#SBATCH --array=1-240
+#SBATCH --time=0-06:00:00 # days-hh:mm:ss
+#SBATCH --mem=8G
+#SBATCH --output=slurmout/fastqc_preprocessed/x_fastqc_preprocessed_%a.out
+#SBATCH --error=slurmout/fastqc_preprocessed/y_fastqc_preprocessed_%a.err
+
+# Load FastQC module
+eval $(spack load --sh fastqc@0.12.1)
+
+# Define input/output directories
+BASEDIR="/scratch/djslab/jltung/Bfrag_project/abx_metagenomes"
+INDIR="${BASEDIR}/downloaded_reads"
+OUTDIR="${BASEDIR}/fastqc_preprocessed"
+
+#Create output directory if it doesn't exist
+mkdir -p ${OUTDIR}
+
+#Get sample names from mapping file
+sample=`sed -n ${SLURM_ARRAY_TASK_ID}p ${BASEDIR}/abx_metagenomes_mappingfile.txt`
+
+# Run FastQC on both forward and reverse reads
+set -x
+time fastqc ${INDIR}/${sample}.trimmo.60.um.1.fastq ${INDIR}/${sample}.trimmo.60.um.2.fastq -o ${OUTDIR}
+RC=$?
+set +x
+
+#Check for success or failure
+if [ $RC -eq 0 ]
+then
+  echo "Job completed successfully for ${sample}"
+else
+  echo "Error Occured in ${sample}!"
+  exit $RC
+fi
